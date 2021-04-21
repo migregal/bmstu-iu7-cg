8d4aa3976ce1d21a631b1558a8045c45563b10a0
migregal <devmigregal@gmail.com>
2021-04-21 12:22:25 +0300

    Canonical alg fixed

--- lab_04/logic/circle_algs/canonical.cpp
+++ lab_04/logic/circle_algs/canonical.cpp
@@ -11,29 +11,29 @@ using namespace std;
 
 void cancircle(std::vector<point_t> &dots, const point_t &center, double r,
                const color_t &color) {
-  auto limit = int32_t(r / std::sqrt(2));
 
-  auto pr = r * r;
-  for (int32_t x = 0; x <= limit; ++x) {
-    auto y = int32_t(sqrt(pr - x * x));
+  auto r_p = r * r;
+  for (int32_t x = 0; x <= int(r / std::sqrt(2)); ++x) {
+    auto y = sqrt(r_p - x * x);
     tmirrored(dots, {x + center.x, y + center.y}, center, color);
   }
 }
 
 void canellipse(std::vector<point_t> &dots, const point_t &c, double ra,
                 double rb, const color_t &color) {
-  auto pa = ra * ra, pb = rb * rb;
 
-  auto limit = int32_t(c.x + pa / std::sqrt(pa + pb));
+  auto ra_p = ra * ra, rb_p = rb * rb;
+  auto limit = int(c.x + ra / std::sqrt(1 + rb_p / ra_p));
+
   for (int32_t x = c.x; x <= limit; ++x) {
-    int32_t y = round(sqrt(1 - x * x / pa) * rb) + c.y;
+    int32_t y = sqrt(ra_p * rb_p - (x - c.x) * (x - c.x) * rb_p) / ra + c.y;
     dmirrored(dots, {x, y}, c, color);
   }
 
-  limit = int(c.y + pb / sqrt(pa + pb));
+  limit = int(c.y + rb / sqrt(1 + ra_p / rb_p));
+
   for (int32_t y = limit; y >= c.y; --y) {
-    int32_t x =
-        round(sqrt(1 - y * y) / rb) + c.x;
+    int32_t x = sqrt(ra_p * rb_p - (y - c.y) * (y - c.y) * ra_p) / rb + c.x;
     dmirrored(dots, {x, y}, c, color);
   }
 }
